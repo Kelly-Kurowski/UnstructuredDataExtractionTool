daa877d949e4f26493be52846e81e8d4a38785f7
kellykurowski <k.kurowski@students.uu.nl>
2024-03-09 15:12:27 +0100

    Added language detection

---
+++ language.py
@@ -0,0 +1,37 @@
+import PyPDF2
+from langdetect import detect
+
+# Detected language code with corresponding Tesseract language codes.
+language_mapping = {
+    'en': 'eng',
+    'es': 'spa',
+    'fr': 'fra',
+    'de': 'deu',
+    'it': 'ita',
+    'nl': 'nld',
+}
+
+
+def detect_file_language(file_path):
+    try:
+        # Open the PDF file
+        with open(file_path, 'rb') as file:
+            # Create a PDF reader object
+            reader = PyPDF2.PdfReader(file)
+
+            # Initialize a variable to store the extracted text
+            text = ""
+
+            # Iterate through each page of the PDF
+            for page_num in range(len(reader.pages)):
+                # Extract text from the current page
+                page = reader.pages[page_num]
+                text += page.extract_text()
+
+        # Detect the language of the text
+        language = detect(text)
+
+        return language, language_mapping[language]
+
+    except Exception as e:
+        return 'en', 'eng'
