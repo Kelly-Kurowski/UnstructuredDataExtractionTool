8dedab7cba1d3fe2727f469f9a6c3a881562d747
Kelly-Kurowski <k.kurowski@students.uu.nl>
2024-05-05 11:35:50 +0200

    Tested optimal batch_size for word length

--- spell_correction.py
+++ spell_correction.py
@@ -15,7 +15,7 @@ def correct_words_batch(spell_checker, words):
     return corrected_batch
 
 
-def correct_misspelled_words(extracted_words: list, lang: str, batch_size: int = 100) -> list:
+def correct_misspelled_words(extracted_words: list, lang: str, batch_size: int = 75) -> list:
     """Correct misspelled words in a list.
 
     Args:
@@ -26,31 +26,29 @@ def correct_misspelled_words(extracted_words: list, lang: str, batch_size: int =
     Returns:
         list: A list of corrected words.
     """
+    start_time = time.time()  # Start the timer
 
-    start_time = time.time()
     # Initialize spell checker once
     spell_checker = SpellChecker(language=lang)
 
-    if len(extracted_words) < 200:
-        batch_size = 60
+    if len(extracted_words) < 220:
+        batch_size = 30
 
     # Calculate the number of batches needed
     num_batches = math.ceil(len(extracted_words) / batch_size)
 
-    # Correct words in each batch
-    corrected_words = []
-    for i in range(num_batches):
-        start_idx = i * batch_size
-        end_idx = min((i + 1) * batch_size, len(extracted_words))
-        batch = extracted_words[start_idx:end_idx]
+    # Correct words in each batch in parallel
+    corrected_batches = Parallel(n_jobs=-1)(
+        delayed(correct_words_batch)(
+            spell_checker, extracted_words[i * batch_size:min((i + 1) * batch_size, len(extracted_words))]
+        ) for i in range(num_batches)
+    )
 
-        # Correct words in the batch
-        corrected_batch = correct_words_batch(spell_checker, batch)
-        corrected_words.extend(corrected_batch)
+    # Flatten the list of lists into a single list
+    corrected_words = [word for batch in corrected_batches for word in batch]
 
-    end_time = time.time()  # Record end time
-    elapsed_time = end_time - start_time  # Calculate elapsed time
-    print("Elapsed time:", elapsed_time, "seconds")  # Print elapsed time
+    end_time = time.time()  # End the timer
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
 
     return corrected_words
-
